e196d93a553887834fc5520d00a9e325aca4030e
d4u0ul <ahb@ecomp.poli.br>
2022-09-05 23:54:08 -0300

    primeiro commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,19 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        //ctrl+alt+o add ou rem imports
+        //ctrl+y deleta linha
+        //refatorar shift+f6
+        //shift+f12 foco no cód
+        //ctrl+alt+l organiza o código
+        //ctrl+d duplica linha
+        //ctrl+shift+seta muda de lugar
+        // mais atalhos http://www.basef.com.br/index.php/Intellij_Idea
+
+
+    }
+}
