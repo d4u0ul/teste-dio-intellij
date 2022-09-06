037e6ca65f5a98047f06279edd21b35a5ffcaf11
d4u0ul <ahb@ecomp.poli.br>
2022-09-05 23:59:13 -0300

    add livros

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -2,6 +2,8 @@ package br.com.dio;
 
 import br.com.dio.model.Gato;
 
+import java.util.Objects;
+
 public class PrimeiroPrograma {
     public static void main(String[] args) {
         Gato gato = new Gato();
@@ -13,7 +15,46 @@ public class PrimeiroPrograma {
         //ctrl+d duplica linha
         //ctrl+shift+seta muda de lugar
         // mais atalhos http://www.basef.com.br/index.php/Intellij_Idea
+        Livro livro1 = new Livro("o teste", 500);
 
+    }
+}
 
+class Livro{
+    private String nome;
+    private int numeroPagina;
+
+    public Livro(String nome, int numeroPagina) {
+        this.nome = nome;
+        this.numeroPagina = numeroPagina;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getNumeroPagina() {
+        return numeroPagina;
+    }
+
+    public void setNumeroPagina(int numeroPagina) {
+        this.numeroPagina = numeroPagina;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Livro livro = (Livro) o;
+        return Objects.equals(nome, livro.nome) && Objects.equals(numeroPagina, livro.numeroPagina);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, numeroPagina);
     }
 }
